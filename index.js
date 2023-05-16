65330ff0206103cc90bae34a449735f74b1e16bc
Adilnur1 <117903971+Adilnur1@users.noreply.github.com>
2023-05-16 08:08:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const str = document.querySelector('.bxs-star');
+
+str.addEventListener("click", () => {
+    str.classList.toggle('active');
+});
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
